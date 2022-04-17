1e76861a49777f1592c9fc44925161d74737bec1
adnanali000 <adnan.museel123@gmail.com>
2022-04-17 17:36:51 +0500

    download section done

--- src/App.js
+++ src/App.js
@@ -23,16 +23,18 @@ const App = () => {
       <SectionWrapper
         title="Deployment"
         description="ProNef is built using Expo which runs natively on all user's devices. You can easily get your app into people's hands."
-        mockupImg={assets.homeHero}
-        banner="banner"
+        mockupImg={assets.feature}
+        reverse
       />
 
       <SectionWrapper
-        title="Smart User Interface Marketplace"
-        description="Experiencce a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
-        mockupImg={assets.homeCards}
-        reverse
+        title="Creative way to showcase the store"
+        description = "The app contains two screens. The fisrt screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
       />
+
+      <Download />
     </>
   );
 }
